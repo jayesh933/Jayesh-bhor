563d1c30f5d066e2e4a88ee7e5438b8e7c0bce9d
jayesh933 <84804475+jayesh933@users.noreply.github.com>
2021-05-25 23:31:20 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,4 @@
+# Write a python program which accepts the radius of circle from user and compute area
+from math import pi
+r=float(input("enter the radius of circle"))
+print("the area of circle is:",str(pi*r**2))
